1e6e5575a1393326b478c88de360aa9a5215160e
Erik Parker <Erik.Parker@cdpehs.com>
2023-07-03 11:28:45 -0500

    Trying to check in all project files

---
+++ LINQ-Fundamentals-Starting/03-Select/Start/Program.cs
@@ -0,0 +1,13 @@
+﻿using LINQSamples;
+
+// Create instance of view model
+SamplesViewModel vm = new();
+
+// Call Sample Method
+// var result = vm.GetSingleColumnMethod();
+// var result = vm.GetSingleColumnQuery();
+// var result = vm.GetSpecificColumnsQuery();
+var result = vm.GetSpecificColumnsMethod();
+
+// Display Results
+vm.Display(result);
